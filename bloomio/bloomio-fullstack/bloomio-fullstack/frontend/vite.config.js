e78c6eb341e08a5d529ec32a2d9bd733a1a9333d
1959414-5 <babzinhaa14@gmail.com>
2026-06-14 19:36:48 -0300

    Define env vars direto no vite.config.js

--- bloomio/bloomio-fullstack/bloomio-fullstack/frontend/vite.config.js
+++ bloomio/bloomio-fullstack/bloomio-fullstack/frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173, host: true },
+  define: {
+    "import.meta.env.VITE_SUPABASE_URL": JSON.stringify("https://eazdyadzsekevyseylug.supabase.co"),
+    "import.meta.env.VITE_SUPABASE_ANON_KEY": JSON.stringify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhemR5YWR6c2VrZXZ5c2V5bHVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyMTI0OTgsImV4cCI6MjA5Njc4ODQ5OH0.xwx0w8l5Q0w5DkwhYjujRK_gSudO6U3wMdTMrpac6no"),
+  },
 });
